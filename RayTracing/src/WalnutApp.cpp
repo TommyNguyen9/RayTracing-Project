cbf2a097184ddd2e0a7ad917fbb92b390ad89866
TommyNguyen9 <tommynguyen3458@gmail.com>
2026-06-12 20:41:43 +0100

    Refactored syntax problems

--- RayTracing/src/WalnutApp.cpp
+++ RayTracing/src/WalnutApp.cpp
@@ -2,6 +2,8 @@
 #include "Walnut/EntryPoint.h"
 
 #include "Walnut/Image.h"
+#include "Walnut/Random.h"
+#include "Walnut/Timer.h"
 
 using namespace Walnut;
 
@@ -11,22 +13,30 @@ public:
 	virtual void OnUIRender() override
 	{
 		ImGui::Begin("Settings");
-		if (ImGui::Button("Render"))
+		ImGui::Text("Last Render: %.3fms", m_LastRenderTime);
+			if (ImGui::Button("Render"))
 		{
 			Render();
 		}
 		ImGui::End();
 
+		ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
 		ImGui::Begin("Viewport");
 
 		m_ViewportWidth = ImGui::GetContentRegionAvail().x;
 		m_ViewportHeight = ImGui::GetContentRegionAvail().y;
 
+		if (m_Image)
+			ImGui::Image(m_Image->GetDescriptorSet(), { (float)m_Image->GetWidth(), (float)m_Image->GetHeight() });
+
 		ImGui::End();
+		ImGui::PopStyleVar();
 	}
 
 	void Render()
 	{
+		Timer timer;
+
 		if (!m_Image || m_ViewportWidth != m_Image->GetWidth() || m_ViewportHeight != m_Image->GetHeight())
 		{
 			m_Image = std::make_shared<Image>(m_ViewportWidth, m_ViewportHeight, ImageFormat::RGBA);
@@ -36,13 +46,21 @@ public:
 
 		for (uint32_t i = 0; i < m_ViewportWidth * m_ViewportHeight; i++)
 		{
-			m_ImageData[i] = 0xffff00ff;
+			m_ImageData[i] = Random::UInt();
+			m_ImageData[i] |= 0xff000000;
 		}
+
+		m_Image->SetData(m_ImageData);
+
+		m_LastRenderTime = timer.ElapsedMillis();
+
 	}
 private:
 	std::shared_ptr<Image> m_Image;
 	uint32_t* m_ImageData = nullptr;
 	uint32_t m_ViewportWidth = 0, m_ViewportHeight = 0;
+
+	float m_LastRenderTime = 0.0f;
 	
 };
 
