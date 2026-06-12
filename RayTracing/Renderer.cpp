982a8945e1a95073bf698ae5a2db09e12d77e528
TommyNguyen9 <tommynguyen3458@gmail.com>
2026-06-12 21:33:48 +0100

    Code refactoring - altering to renderer class

--- RayTracing/Renderer.cpp
+++ RayTracing/Renderer.cpp
@@ -1,4 +1,5 @@
 #include "Renderer.h"
+#include "Walnut/Random.h"
 
 void Renderer::Render()
 {
@@ -7,20 +8,31 @@ void Renderer::Render()
 
 void Renderer::OnResize(uint32_t width, uint32_t height)
 {
-	if (!m_Image)
+	if (m_FinalImage)
 	{
 		// No resize needed
-		if (m_Image->GetWidth() == width && m_Image->GetHeight() == height)
+		if (m_FinalImage->GetWidth() == width && m_FinalImage->GetHeight() == height)
 			return;
 
-		m_Image->Resize(width, height);
+		m_FinalImage->Resize(width, height);
 	}
 	else
 	{
-		m_Image = std::make_shared<Walnut::Image>(width, height, Walnut::ImageFormat::RGBA);
+		m_FinalImage = std::make_shared<Walnut::Image>(width, height, Walnut::ImageFormat::RGBA);
 	}
 
 	delete[] m_ImageData;
 	m_ImageData = new uint32_t[width * height];
 
+}
+
+void Renderer::Render()
+{
+	for (uint32_t i = 0; i < m_FinalImage->GetWidth() * m_FinalImage->GetHeight(); i++)
+	{
+		m_ImageData[i] = Walnut::Random::UInt();
+		m_ImageData[i] |= 0xff000000;
+	}
+
+	m_FinalImage->SetData(m_ImageData);
 }
