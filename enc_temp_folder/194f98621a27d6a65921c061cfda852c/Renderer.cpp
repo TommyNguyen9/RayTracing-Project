49b0a0663e55b7fb1dab47558db9641162065154
TommyNguyen9 <tommynguyen3458@gmail.com>
2026-06-18 17:20:02 +0100

    Included pcg hash function

--- enc_temp_folder/194f98621a27d6a65921c061cfda852c/Renderer.cpp
+++ enc_temp_folder/194f98621a27d6a65921c061cfda852c/Renderer.cpp
@@ -16,13 +16,6 @@ namespace Utils {
 		uint32_t result = (a << 24) | (b << 16) | (g << 8) | r;
 		return result;
 	}
-
-	static uint32_t PCG_Hash(uint32_t input)
-	{
-		uint32_t state = input * 747796405u + 2891336453u;
-		uint32_t word = ((state >> ((state >> 28u) + 4u)) ^ state) * 277883737u;
-		return (word >> 22u) ^ word;
-	}
 }
 
 void Renderer::OnResize(uint32_t width, uint32_t height)
